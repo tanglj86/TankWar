29495a20372d5d90cfbe4973e8638d0c10dacca5
tanglj86 <tanglj86@gmail.com>
2015-11-22 16:46:04 +0800

    添加重画paint方法,并画出tank

--- src/basic/TankClient.java
+++ src/basic/TankClient.java
@@ -1,21 +1,23 @@
 package basic;
 
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
-public class TankClient extends Frame{
+public class TankClient extends Frame {
 
 	public static void main(String[] args) {
 		TankClient tc = new TankClient();
 		tc.lauchFrame("TankWar");
 	}
-	
-	public void lauchFrame(String name){
+
+	public void lauchFrame(String name) {
 		setTitle(name);
 		setLocation(50, 50);
 		setSize(800, 600);
+		setBackground(Color.GREEN);
 		setResizable(false);
 		setVisible(true);
 		this.addWindowListener(new WindowAdapter() {
@@ -24,8 +26,23 @@ public class TankClient extends Frame{
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
-			
+
 		});
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.Window#paint(java.awt.Graphics)
+	 */
+	@Override
+	public void paint(Graphics g) {
+		Color oldColor = g.getColor();
+		g.setColor(Color.RED);
+
+		g.fillOval(50, 50, 30, 30);
+		// 设置回原来的颜色
+		g.setColor(oldColor);
 	}
 
 }
