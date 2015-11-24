c081e3d8476a2a57feaa58fe331c401e2560d854
tanglj86 <tanglj86@gmail.com>
2015-11-24 15:15:47 +0800

    1.通过Toolkit加载爆炸图片

--- src/basic/Explode.java
+++ src/basic/Explode.java
@@ -2,14 +2,40 @@ package basic;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 public class Explode {
 	int x, y;
 	private boolean live = true;
 
+	private static Toolkit tk = Toolkit.getDefaultToolkit();
+	private static Image[] images = {
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/0.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/1.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/2.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/3.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/4.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/5.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/6.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/7.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/8.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/9.gif")),
+			tk.getImage(Explode.class.getClassLoader().getResource(
+					"images/10.gif")) };
 	private TankClient tc;
 
-	int[] diameter = { 4, 7, 12, 18, 26, 32, 14, 6 };
+	// int[] diameter = { 4, 7, 12, 18, 26, 32, 14, 6 };
 	int step = 0;
 
 	public Explode(int x, int y, TankClient tc) {
@@ -24,7 +50,7 @@ public class Explode {
 			return;
 		}
 
-		if (step == diameter.length) {
+		if (step == images.length) {
 			live = false;
 			step = 0;
 			return;
@@ -32,7 +58,8 @@ public class Explode {
 
 		Color c = g.getColor();
 		g.setColor(Color.ORANGE);
-		g.fillOval(x, y, diameter[step], diameter[step]);
+		// g.fillOval(x, y, diameter[step], diameter[step]);
+		g.drawImage(images[step], x, y, null);
 		g.setColor(c);
 
 		step++;
